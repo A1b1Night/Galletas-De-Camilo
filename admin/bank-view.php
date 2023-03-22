6e96c53c57d86513be84a86146d55ccd593322e3
A1Jhonas <carlos_55232@hotmail.com>
2023-03-22 13:38:45 -0500

    Galletas de Camilo

---
+++ admin/bank-view.php
@@ -0,0 +1,86 @@
+<div class="container">
+    <div class="row">
+        <div class="col-xs-12">
+            <?php
+                $bankAccount=ejecutarSQL::consultar("SELECT * FROM cuentabanco");
+                if(mysqli_num_rows($bankAccount)>=1){
+                  $bankD=mysqli_fetch_array($bankAccount, MYSQLI_ASSOC);
+            ?>
+                  <div class="container-form-admin">
+                  <form action="./process/updateBank.php" method="POST" role="form" class="form-content FormCatElec" data-form="update">
+                    <h3 class="text-info text-center">Actualizar Cuenta de Banco</h3>
+                    <input type="hidden" name="id" value="<?php echo $bankD['id']; ?>">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Número de Cuenta</label>
+                                    <input class="form-control" type="text" name="bancoCuenta" value="<?php echo $bankD['NumeroCuenta']; ?>" maxlength="50" required="">
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Nombre del Banco</label>
+                                    <input class="form-control" type="text" name="bancoNombre" value="<?php echo $bankD['NombreBanco']; ?>" maxlength="50" required="">
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Nombre del Beneficiario</label>
+                                    <input class="form-control" type="text" name="bancoBeneficiario" value="<?php echo $bankD['NombreBeneficiario']; ?>" maxlength="50" required="">
+                                </div>
+                            </div>
+                            <div class="col-xs-12 col-sm-6">
+                                <div class="form-group label-floating">
+                                    <label class="control-label">Tipo de Cuenta</label>
+                                    <input class="form-control" type="text" name="bancoTipo" value="<?php echo $bankD['TipoCuenta']; ?>" maxlength="50" required="">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <p class="text-center"><button type="submit" class="btn btn-success btn-raised">Actualizar Cuenta de Banco</button></p>
+                  </form>
+                  </div>
+            <?php
+                }else{
+            ?>
+              <div class="container-form-admin">
+              <form action="./process/regBank.php" method="POST" role="form" class="FormCatElec" data-form="save">
+                <h3 class="text-info text-center">Agregar Cuenta de Banco</h3>
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-xs-12 col-sm-6">
+                            <div class="form-group label-floating">
+                                <label class="control-label">Numero de Cuenta</label>
+                                <input class="form-control" type="text" name="bancoCuenta" maxlength="50" required="">
+                            </div> 
+                        </div>
+                        <div class="col-xs-12 col-sm-6">
+                            <div class="form-group label-floating">
+                                <label class="control-label">Nombre del Banco</label>
+                                <input class="form-control" type="text" name="bancoNombre" maxlength="50" required="">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6">
+                            <div class="form-group label-floating">
+                                <label class="control-label">Nombre del Beneficiario</label>
+                                <input class="form-control" type="text" name="bancoBeneficiario" maxlength="50" required="">
+                            </div>
+                        </div>
+                        <div class="col-xs-12 col-sm-6">
+                            <div class="form-group label-floating">
+                                <label class="control-label">Tipo de Cuenta</label>
+                                <input class="form-control" type="text" name="bancoTipo" maxlength="50" required="">
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <p class="text-center"><button type="submit" class="btn btn-primary btn-raised">Agregar Cuenta de Banco</button></p>
+              </form>
+              </div>
+            <?php
+                }
+            ?>
+        </div>
+    </div>
+</div>
---
+++ inc/footer.php
@@ -0,0 +1,44 @@
+
+
+<div class="ResForm"></div>
+<div class="ResbeforeSend"></div>
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4">
+                <h4 class="text-footer">Redes Sociales</h4>
+                <a href="https://www.facebook.com/lasgalletasdecamilo" target="_blank">
+                    <i class="fab fa-facebook" aria-hidden="true">&nbsp; Facebook </i> 
+                </a><br>
+                <a href="https://twitter.com/?lang=es" target="_blank">
+                    <i class="fab fa-twitter" aria-hidden="true">&nbsp; Twitter </i>
+                </a><br>
+                <a href="https://www.instagram.com/lasgalletasdecamilo/" target="_blank">
+                    <i class="fab fa-instagram" aria-hidden="true">&nbsp; Instagram </i>
+                </a><br>
+                <a href="https://linktr.ee/lasgalletasdecamilo" target="_blank">
+                    <i class="fa fa-list" aria-hidden="true">&nbsp; Encuentranos </i>
+                </a>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer">Nuestro Compromiso</h4>
+                <p> 
+                    En las Galletas de Camilo tenemos el compromiso</br>
+                    de darle siempre a tu mascota los productos</br>
+                    en su forma más nutritiva, sin preservantes</br>
+                    dañinos ni saborizantes, sales o azúcares artificiales.</br>
+                </p>
+            </div>
+            <div class="col-sm-4">
+                <h4 class="text-footer" >Dirección</h4>
+                <p style="color: #FFF">Av. Alfredo Benavides 347, piso 2</p>
+                <p style="color: #FFF">Miraflores - Lima - Perú</p>
+                <p style="font-size:20px" aria-hidden="true">  995 326 143</p>
+                <p class="fas fa-mobile-alt" style="font-size:20px"  aria-hidden="true"> 936 093 538 </p><br> <a href="https://outlook.live.com/" target="_blank" style="color: #5bc0de">envianos_un_mensaje</a></br>
+                E-mail: <a href="https://linktr.ee/lasgalletasdecamilo" target="_blank" style="color: #5bc0de"> &nbsp; linktr.ee/lasgalletasdecamilo</a>
+            </div>
+        </div>
+    </div>
+    <br><br><br>
+    <h5 class="text-center tittles-pages-logo text-footer"><i class="fas fa-copyright"> Copyright Universidad de Ciencias y Humanidades</i> 2021</h5>
+</footer>
